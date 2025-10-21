e2ad82f8531ee071d2f04d5f88be6b87e9cd5dd8
yaumil <yaumilramkhadani@gmail.com>
2025-10-21 11:16:41 +0700

    menambahkan routing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,12 +19,16 @@ export default function Navbar() {
 
         {/* Action */}
         <div className="hidden md:flex gap-3">
-          <button className="bg-white text-purple-700 font-semibold px-4 py-2 rounded-lg hover:bg-yellow-200 transition">
-            Masuk
-          </button>
+           <Link to="/login">
+            <button className="bg-white text-purple-700 font-semibold px-4 py-2 rounded-lg hover:bg-yellow-200 transition">
+              Masuk
+            </button>
+          </Link>
+          <Link to="/register">
           <button className="bg-yellow-300 text-purple-800 font-semibold px-4 py-2 rounded-lg hover:bg-yellow-400 transition">
             Daftar
           </button>
+          </Link>
         </div>
       </div>
 
